4014fee4e9aac19a9441580f8e810e7dba887ac2
Annie Lydens <aelydens@gmail.com>
2024-10-25 16:07:16 -0400

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-300 container mx-auto flex items-center justify-center p-8">
+      <div className="bg-gray-700 flex items-center justify-center w-[600px] h-[600px] p-6">
+        Space Invaders go here
+      </div>
+    </div>
+  );
+}
+
+export default App;
